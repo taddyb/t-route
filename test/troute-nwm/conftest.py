68afc27c16d2195a57e5c7eb963faa6fe1769d53
Tadd.Bindas <Tadd.N.Bindas@rtx.com>
2024-10-29 16:46:26 -0400

    finished nhd tests

--- test/troute-nwm/conftest.py
+++ test/troute-nwm/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 import pandas as pd
-import numpy as np
 from pathlib import Path
 
 @pytest.fixture
@@ -9,3 +8,9 @@ def expected_nhd_preprocessed_outputs():
     return {
         'qlats': pd.read_csv(cwd / "test/troute-nwm/sample_outputs/nhd/qlats.csv"),
     }
+
+@pytest.fixture
+def expected_q0():
+    cwd = Path.cwd()
+    expected_q0 = pd.read_parquet(cwd / "test/troute-nwm/sample_outputs/nhd/q0_nwm_route_results.parquet")
+    return expected_q0
